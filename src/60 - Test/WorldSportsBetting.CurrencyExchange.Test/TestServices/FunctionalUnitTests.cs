1da88e6ef4446c9c0a42c9e34356b9c54fab7212
ASUS-TUF-GAMING\Tshidiso <Tshidiso.Mathabe@daklapack.com>
2024-08-02 07:28:44 +0200

    add more unit tests

--- src/60 - Test/WorldSportsBetting.CurrencyExchange.Test/TestServices/FunctionalUnitTests.cs
+++ src/60 - Test/WorldSportsBetting.CurrencyExchange.Test/TestServices/FunctionalUnitTests.cs
@@ -1,4 +1,5 @@
 using Moq;
+using Newtonsoft.Json;
 using WorldSportsBetting.CurrencyExchange.Core.Services;
 using WorldSportsBetting.CurrencyExchange.Domain.DataTransferObjects;
 
@@ -39,6 +40,55 @@ namespace WorldSportsBetting.CurrencyExchange.Test.TestServices
 
             // Assert
             Assert.AreEqual(expectedResult, actualResult.Result);
+        }
+
+        [TestMethod]
+        public async Task When_ConvertAsyncIsInvoked_Given_TheCurrenyIsNotAvailable_Should_GracefullyErrorHandle_Async()
+        {
+            // Arrange
+            string baseCurrency = "USD";
+            string targetCurrency = "StillNotHotBadCurrency";
+            string expectedResult = $"'{targetCurrency}' currency is not available.";
+            LatestResponseDto latestResponseDto =
+                new()
+                {
+                    Base = baseCurrency,
+                    Rates = new Dictionary<string, double> { ["TestCurrency"] = 1, ["TestCurrency1"] = 8.5, ["TestCurrency2"] = 13, ["TestCurrency3"] = 5.1 }
+                };
+            _currencyRatesServiceMock.Setup(x => x.GetLatestCurrencyRatesAsync(baseCurrency, default)).Returns(Task.FromResult(latestResponseDto));
+            ConvertService convertService = new(_currencyRatesServiceMock.Object);
+
+            // Act
+            CalcConvertResponseDto actualResult = await convertService.ConvertAsync(baseCurrency, targetCurrency, 13, default);
+
+            // Assert
+            Assert.IsFalse(actualResult.Success);
+            Assert.AreEqual(expectedResult, actualResult.ErrorMessage);
+        }
+
+        [TestMethod]
+        public async Task When_ConvertAsyncIsInvoked_Given_ThereIsAnError_Should_GracefullyErrorHandle_Async()
+        {
+            // Arrange
+            string baseCurrency = "ZAR";
+            string targetCurrency = "ItWasLateTiredBadCurrency";
+            LatestResponseDto latestResponseDto =
+                new()
+                {
+                    Base = baseCurrency,
+                    HasError = true,
+                    ErrorDetails = new LatestErrorResponseDto { Message = "Exception", Result = "Third Party API downtime schedule." }
+                };
+            string expectedErrorDetailsResult = JsonConvert.SerializeObject(latestResponseDto.ErrorDetails);
+            _currencyRatesServiceMock.Setup(x => x.GetLatestCurrencyRatesAsync(baseCurrency, default)).Returns(Task.FromResult(latestResponseDto));
+            ConvertService convertService = new(_currencyRatesServiceMock.Object);
+
+            // Act
+            CalcConvertResponseDto actualResult = await convertService.ConvertAsync(baseCurrency, targetCurrency, 13, default);
+
+            // Assert
+            Assert.IsFalse(actualResult.Success);
+            Assert.AreEqual(expectedErrorDetailsResult, actualResult.ErrorMessage);
         }
     }
 }
